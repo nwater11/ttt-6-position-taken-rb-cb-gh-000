b36a54e20c2342dee9c8658409568b355b215e3f
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-14 19:27:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 def position_taken?(array, index)
-  if array[index] == " " || array[index] == ""
+  if array[index] == " " || array[index] == "" || array[index] == nil
     false
+  else
+    true  
   end
 end# code your #position_taken? method here!
